a7ffedb3ac2a3cdd1be5d86f34b9825740604898
Nivedhass <128232040+Nivedhass@users.noreply.github.com>
2024-09-17 14:53:02 +0530

    Update script.js

--- calculator_project/script.js
+++ calculator_project/script.js
@@ -1,20 +1,17 @@
-// Function to insert numbers and operators into the screen
+
 function insert(num) {
     document.getElementById('result').value += num;
 }
 
-// Function to clear the screen
 function clearScreen() {
     document.getElementById('result').value = '';
 }
 
-// Function to delete the last character
 function deleteLast() {
     let value = document.getElementById('result').value;
     document.getElementById('result').value = value.substring(0, value.length - 1);
 }
 
-// Function to calculate the result
 function calculate() {
     try {
         let expression = document.getElementById('result').value;
